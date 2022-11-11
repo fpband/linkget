6c78bcc8fd7d390f6f3f5170c358509df51cfcd9
fpband <82159763+fpband@users.noreply.github.com>
2022-11-11 09:24:03 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int( os.getenv("api_id","3335796") )
     API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001249461809") )
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001645543494") )
     CHANNEL_USERNAME = os.getenv("channel_username","dlchin")
     TOKEN = os.getenv("token","2147458930:AAEpjUlRVk87oK_jsqCOAyvGhLfAh3hFybg")
-    DOMAIN  = os.getenv("domain","https://newdlstar.scalingo.com")
+    DOMAIN  = os.getenv("domain","https://newsfastlink.scalingo.com")
