97e4c99592c6d242ed1a33e5e61fdd419eacbaa8
fpband <82159763+fpband@users.noreply.github.com>
2022-11-11 09:21:15 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb701cb")
+    API_ID = int( os.getenv("api_id","3335796") )
+    API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
     CHANNEL = int( os.getenv("channel_files_chat_id","-1001249461809") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","xxxxx")
-    DOMAIN  = os.getenv("domain","https://newdlstar.herokuapp.com")
+    CHANNEL_USERNAME = os.getenv("channel_username","dlchin")
+    TOKEN = os.getenv("token","2147458930:AAEpjUlRVk87oK_jsqCOAyvGhLfAh3hFybg")
+    DOMAIN  = os.getenv("domain","https://newdlstar.scalingo.com")
