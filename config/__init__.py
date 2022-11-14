a66a3dce9910e543e3c9d441908bccaa2dec89f8
fpband <82159763+fpband@users.noreply.github.com>
2022-11-14 22:04:34 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -6,4 +6,4 @@ class Config:
     CHANNEL = int( os.getenv("channel_files_chat_id","-1001645543494") )
     CHANNEL_USERNAME = os.getenv("channel_username","dlchin")
     TOKEN = "1996499923:AAGVCjT4msY1AOHb-qtalwMsrwu0dD65k7g"
-    DOMAIN  = os.getenv("domain","https://newfastlink70.osc-fr1.scalingo.io/")
+    DOMAIN  = os.getenv("domain","https://newfastlink70.osc-fr1.scalingo.io")
